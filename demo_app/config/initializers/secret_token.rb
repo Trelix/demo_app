1e794847c0d55198be867308aad1531ce343d1ed
Trelix <trelix@mail.ru>
2013-03-29 13:12:20 +0400

    Initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3f99c691ce836f8e9a7fca453463f1a4d99de9987f60606e19f29489221529a49a9a6ce9409226845cbdb6c9d6084315777cf01e9a402e589a6a2d61137fb7ba'
---
+++ trelixdb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trelixdb::Application.config.secret_token = 'e9897b48059308f4c54d3acfdf084e85d88d7dbaa39562cf52f9670b1205071ede1d45b7acee2ac36c4ae5066650f1e6b40b3c0f6f036c9a6e32230bcebf3202'
